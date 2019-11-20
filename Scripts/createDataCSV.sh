223a84767a0f1fd33cb72356378babd52224196d
Zoe Bernard <31932066+ZoeBernard@users.noreply.github.com>
2019-11-20 16:44:33 +0100

    Added scripts that make datafile

---
+++ Scripts/createDataCSV.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+pantheria="/home/zoe/Documents/GitHub/trait-organismal-ungulates/data/PanTHERIA.tsv"
+species="/home/zoe/Documents/GitHub/trait-organismal-ungulates/data-selfmade/speciesList.txt"
+panHeader="/home/zoe/Documents/GitHub/trait-organismal-ungulates/data-selfmade/headers.txt"
+traitCSV="/home/zoe/Documents/GitHub/trait-organismal-ungulates/data-selfmade/ungulatesTraits.csv"
+
+# Extract headers from Pantheria file, remove Binomial column
+header=$(head -n 1 ${pantheria} | sed -e 's/\<MSW05_Binomial\>//g'| awk '{for(i=0;i<=35;++i)print $i}' | tail -n 35 | tr '\n' ',' | sed -e 's/\<References\>//g' | sed '$ s/.$//' |  sed '$ s/.$//')
+
+
+# Declare the headers
+# headerTaxonomy: contains the BinomialName, Order, Family, Genus and Species
+# headerID: contains the headers for the ID-codes
+# headerTraits1: contains the traits from the Pantheria file
+# headerTraits2: contains the chosen traits
+# headerTraits3: contains the traits for the Seshat Database
+headerTaxonomy=$(echo ${header} | cut -d ',' -f -4 | tr ',' ' ' | sed 's/\MSW05_//g' | tr ' ' ',')
+headerID=$(echo 'ID_EoL')
+headerTraits1=$(echo ${header} | cut -d ',' -f 5-)
+headerTraits2=$(echo 'Diet,AVGFoodConsumption,Sociality,SocialHierarchy,NumMales,GestationPeriod,MaturityReach,MatingSystem,NumOffspring,BreedingInterval,YearRoundBreeding,AVGWeight,DevelopmentStrategy,Horns/Antler,Topspeed,Lifespan,ActivityCycle,NaturalPredators')
+headerTraits3=$(echo 'CarryWeight,PullStrength,AVGMovingSpeed,AVGTravelDistance')
+
+
+# Add new columns
+printf "%s,%s,%s,%s,%s,%s,%s" "${headerID}" "BinomialName" "${headerTaxonomy}" "Domestication" "${headerTraits1}" "${headerTraits2}" "${headerTraits3}" > ${panHeader}
+
+
+# Combine the new header file with the species
+cat ${panHeader} ${species} > ${traitCSV}
+
+
+
+
---
+++ Scripts/createSpeciesList.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+
+# Declaring variables
+ungulatesTree="/home/zoe/Documents/GitHub/trait-organismal-ungulates/data/ungulates.tree"
+numTree="/home/zoe/Documents/GitHub/trait-organismal-ungulates/data/S21191.tree"
+pantheria="/home/zoe/Documents/GitHub/trait-organismal-ungulates/data/PanTHERIA.tsv"
+idfile="/home/zoe/Documents/GitHub/trait-organismal-ungulates/data/id.csv"
+speciesTXT="/home/zoe/Documents/GitHub/trait-organismal-ungulates/data-selfmade/speciesList.txt"
+
+binomArray=()
+orderArray=()
+famArray=()
+genusArray=()
+speciesArray=()
+domArray=()
+pantArray1=()
+pantArray2=()
+pantArrayfinal=()
+eolIDarray=()
+echo "" > "${speciesTXT}"
+
+
+# Get species names
+# Make a list of the species names from the ungulates.tree file
+sed 's/[0-9]*//g' ${ungulatesTree} | tr -d "():;." | tr "," "\n" > ungulateSpecies.txt
+
+# Make a list of the species names from the S21191.tree file
+cat ${numTree} | sed 's/[0-9]*//g' | tr -d "():;." | tr "," "\n" > S21191Species.txt
+
+# Combine the two lists and remove duplicates
+speciesList=$(cat ungulateSpecies.txt S21191Species.txt | sort -u | sort)
+
+# Delete obsolete files
+rm ungulateSpecies.txt S21191Species.txt
+
+
+# Loop through speciesList and look up the orders from PanTHERIA
+# Needed orders: Artiodactyla (even-toed), Perissodactyla (uneven-toed) and Proboscidea (elephants)
+for species in ${speciesList}
+do	
+	grepname=$(echo ${species} | sed 's/_/ /g')
+	order=$(grep "${grepname}" ${pantheria} | awk '{printf $1}')
+
+	if [[ ${order} == "Artiodactyla" ]]  || [[ ${order} == "Perissodactyla" ]] ||  [[ ${order} == "Proboscidea" ]]
+	then
+		binomArray+=(${species})
+		orderArray+=(${order})
+
+		fam=$(grep "${grepname}" ${pantheria} | awk '{printf $2}')
+		famArray+=(${fam})
+
+		genus=$(grep "${grepname}" ${pantheria} | awk '{printf $3}')
+		genusArray+=(${genus})
+
+		species=$(grep "${grepname}" ${pantheria} | awk '{printf $4}')
+		speciesArray+=(${species})
+		
+		domArray+=("X")
+
+		pant=$(grep "${grepname}" ${pantheria} | cut -f6-35 | tr "\t" "," | tr -d "\n")
+		pantArray1+=(${pant})
+
+		# Get ID from EoL file
+		eol=$(grep "${grepname}" ${idfile} | head -n 1 | tr ',' ' ' | awk '{printf $1}')
+		eolIDarray+=(${eol})
+	fi
+done
+
+
+
+# Write the newly found species to a txt file
+for (( i=0; i<=${#binomArray[@]}; i++ ))
+do
+	printf "%s,%s,%s,%s,%s,%s,%s," "${eolIDarray[${i}]}" "${binomArray[${i}]}" "${orderArray[${i}]}" "${famArray[${i}]}" "${genusArray[${i}]}" "${speciesArray[${i}]}" "${domArray[${i}]}" >> ${speciesTXT}
+
+	pantArray2=$(echo ${pantArray1[${i}]})
+	for (( y=0; y<=35; y++ ))
+	do
+		printf "%s," ${pantArray2[${y}]} >> ${speciesTXT}
+	done
+	printf "\n" >> ${speciesTXT}
+
+done
+
+sed -i '$d' ${speciesTXT}
