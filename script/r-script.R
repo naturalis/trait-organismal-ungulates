510764e8ae103d977db29564acea1c9702c71e64
Zoe Bernard <31932066+ZoeBernard@users.noreply.github.com>
2020-05-11 17:37:00 +0200

    Added summary and tried making some boxplots with a small part of the dataset

--- script/r-script.R
+++ script/r-script.R
@@ -1,14 +1,29 @@
+# Load packages
 library(ape)
 library(ggtree)
+library(ggplot2)
 
+
+# Declare root and paths to data
 root <- "/home/zoe/Documents/Naturalis/R-Studio/trait-organismal-ungulates"
 treeFile <- paste(c(root, "/data/ungulates.tree"), collapse = "")
 ungulatesFile <- paste(c(root, "/data/CSV/ungulatesTraits.csv"), collapse = "")
 
+
+# Read datafiles into R
 phyloTree <- read.tree(treeFile)
 ggtree(phyloTree, layout = "circular") 
-# + geom_tiplab() adds labels, but doesn't work (yet) 
+# + geom_tiplab() adds labels, but doesn't work properly (yet) 
 
 ungulatesData <- read.csv(ungulatesFile, header = TRUE, sep= ",")
 
 
+# Information about data per predictor variable
+summary(ungulatesData)
+str(ungulatesData)
+ungulatesData$BinomialName
+
+# Testdata
+theme_set(theme_bw(base_size=18))
+testdata <- read.csv(ungulatesFile, header = TRUE, sep = ",", nrows = 10)
+qplot(testdata$Domestication, testdata$X1.1_ActivityCycle, facets = . ~ testdata$BinomialName, colour = BinomialName, geom = "boxplot", data = testdata)
