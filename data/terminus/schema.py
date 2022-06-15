d732d65d2a3d7f7af94c7b132b4a4b47cd80f638
Rutger Vos <rutgeraldo@gmail.com>
2022-06-15 18:55:02 +0200

    first CSV data push

--- data/terminus/schema.py
+++ data/terminus/schema.py
@@ -5,93 +5,71 @@
 # use 'terminusdb sync' to change this file according to
 # the exsisting database schema
 ####
-"""
-Title: This is an Example
-Description: Example to show how schema works
-Authors: TerminusDB, Cheuk
-"""
-from typing import List, Optional, Set
+from typing import Optional
 
 from terminusdb_client.woqlschema import (
     DocumentTemplate,
-    EnumTemplate,
-    HashKey,
-    TaggedUnion,
 )
 
 
-class Country(DocumentTemplate):
-    """This is Country.
-
-    Country is a class object in the schema. It's class attributes will be the properties of the object. Therefore, a Country object will have a name which is string and a list of alias names that is called 'also_know_as'
-    """
-
-    _key = HashKey(["name"])  # Specifies a specific key generation method to use
-    name: str
-    also_know_as: List[str]  # Can be a list
-
-
-class Address(DocumentTemplate):
-    """This is address"""
-
-    _subdocument = (
-        []
-    )  # Subdocument means that it will not be reference when added as a property of another object
-    street: str
-    postal_code: str
-    country: Country  # Type can be a Class that is defined
-
-
-class Person(DocumentTemplate):
-    """This is a person
-
-    Can store the explanation to the attributes in the docstring. Docstrings needs to be in numpydoc format.
-
-    Attributes
-    ----------
-    name : str
-        Name of the person.
-    age : int
-        Age of the person.
-    """
-
-    name: str
-    age: int
-    friend_of: Set["Person"]  # Using quotation for future reference
-
-
-class Employee(Person):
-    """Employee will inherits the attributes from Person"""
-
-    address_of: Address
-    contact_number: Optional[str]  # if Optional is used, it can be None
-    managed_by: "Employee"
-
-
-class Coordinate(DocumentTemplate):
-    _abstract = []  # Abstract means that it cannot have any instances
-    x: float
-    y: float
-
-
-class Location(Address, Coordinate):
-    """Location is inherits from Address and Coordinate
-
-    Class can have multiple inheritance. It will inherits both the attibutes from Address and Coordinate.
-    """
-
-    name: str
-
-
-class Team(EnumTemplate):
-    """This is an example for Enum, if a value is not provided, the name of the Enum (e.g. Marketing) will be used as the value."""
-
-    IT = "Information Technology"
-    Marketing = ()
-
-
-class Contact(TaggedUnion):
-    """TaggedUnion allow options for types"""
-
-    local_number: int
-    international: str
+class UngulatesTraits(DocumentTemplate):
+    avgbodymass: Optional[float]
+    avgfoodconsumption: Optional[float]
+    avgmovingspeed: Optional[float]
+    avgtraveldistance: Optional[float]
+    binomialname: Optional[str]
+    breedinginterval: Optional[float]
+    canonicalname: Optional[str]
+    carryweight: Optional[float]
+    developmentstrategy: Optional[float]
+    diet: Optional[float]
+    domestication: Optional[int]
+    family: Optional[str]
+    genus: Optional[str]
+    headornaments: Optional[float]
+    id_eol: Optional[int]
+    lifespan: Optional[float]
+    matingsystem: Optional[float]
+    maturityreachfemale: Optional[float]
+    maturityreachmale: Optional[float]
+    motility: Optional[float]
+    naturalpredators: Optional[float]
+    nummales: Optional[float]
+    numoffspring: Optional[float]
+    order: Optional[str]
+    parentalcare: Optional[float]
+    pullstrength: Optional[float]
+    socialhierarchy: Optional[float]
+    sociality: Optional[float]
+    species: Optional[str]
+    x10_1_populationgrpsize: Optional[float]
+    x10_2_socialgrpsize: Optional[float]
+    x12_1_habitatbreadth: Optional[float]
+    x12_2_terrestriality: Optional[float]
+    x13_1_adultheadbodylen_mm: Optional[float]
+    x13_2_neonateheadbodylen_mm: Optional[float]
+    x13_3_weaningheadbodylen_mm: Optional[float]
+    x14_1_interbirthinterval_d: Optional[float]
+    x15_1_littersize: Optional[float]
+    x16_1_littersperyear: Optional[float]
+    x17_1_maxlongevity_m: Optional[float]
+    x18_1_basalmetrate_mlo2hr: Optional[float]
+    x1_1_activitycycle: Optional[float]
+    x21_1_populationdensity_n_km2: Optional[float]
+    x22_1_homerange_km2: Optional[float]
+    x22_2_homerange_indiv_km2: Optional[float]
+    x23_1_sexualmaturityage_d: Optional[float]
+    x24_1_teatnumber: Optional[float]
+    x25_1_weaningage_d: Optional[float]
+    x2_1_ageateyeopening_d: Optional[float]
+    x3_1_ageatfirstbirth_d: Optional[float]
+    x5_1_adultbodymass_g: Optional[float]
+    x5_2_basalmetratemass_g: Optional[float]
+    x5_3_neonatebodymass_g: Optional[float]
+    x5_4_weaningbodymass_g: Optional[float]
+    x6_1_dietbreadth: Optional[float]
+    x6_2_trophiclevel: Optional[float]
+    x7_1_dispersalage_d: Optional[float]
+    x8_1_adultforearmlen_mm: Optional[float]
+    x9_1_gestationlen_d: Optional[float]
+    yearroundbreeding: Optional[float]
